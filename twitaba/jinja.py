4a7e5797f4abe486d2d77f04573d9e23cd6e89a0
Alexander Pushkov <alexander@notpushk.in>
2019-01-28 22:42:11 +0300

    Update to v2 of conversation API

--- twitaba/jinja.py
+++ twitaba/jinja.py
@@ -2,7 +2,6 @@ import jinja2
 from sanic import response
 
 from .util import get_banner
-from .twitter import prepare_post
 
 
 j2 = jinja2.Environment(
@@ -15,9 +14,7 @@ j2.globals.update({"len": len, "get_banner": get_banner})
 
 def renderpost_filter(t, show_thread_link=False):
     return jinja2.Markup(
-        j2.get_template("post.html").render(
-            show_thread_link=show_thread_link, **t
-        )
+        j2.get_template("post.html").render(show_thread_link=show_thread_link, **t)
     )
 
 
--- twitaba/twitter.py
+++ twitaba/twitter.py
@@ -1,9 +1,23 @@
 import asyncio
+import requests
 
 from ttp import ttp
 
 
-async def fetch_home_threads(session):
+USER_AGENT = (
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Ubuntu Chromium/71.0.3578.98 Chrome/71.0.3578.98 Safari/537.36"
+)
+
+
+def get_guest_token():
+    r = requests.get(
+        "https://mobile.twitter.com/jack", headers={"user-agent": USER_AGENT}
+    )
+    return r.text.rsplit('("gt=', 1)[1].split(";")[0]
+
+
+def get_home_thread_ids(session):
     r = session.get(
         "https://api.twitter.com/1.1/statuses/home_timeline.json",
         params={"count": 40, "trim_user": True, "include_entities": False},
@@ -21,72 +35,91 @@ async def fetch_home_threads(session):
         if thread_id not in thread_ids:
             thread_ids.append(thread_id)
 
+    return thread_ids
+
+
+async def fetch_home_threads(session):
+    thread_ids = get_home_thread_ids(session)
+
+    guest_token = get_guest_token()
+
+    def except_pass(f, *a, **kw):
+        try:
+            return f(*a, **kw)
+        except Exception as e:
+            print(repr(e), a, kw)
+            pass
+
     loop = asyncio.get_event_loop()
     futures = [
-        loop.run_in_executor(None, fetch_thread, session, thread_id)
+        loop.run_in_executor(None, except_pass, fetch_thread, thread_id, guest_token)
         for thread_id in thread_ids
     ]
 
-    return await asyncio.gather(*futures)
+    return [x for x in await asyncio.gather(*futures) if x is not None]
 
 
-def fetch_thread(session, thread_id):
-    r = session.get(
-        "https://api.twitter.com/1.1/conversation/show/%i.json?tweet_mode=extended"
-        % thread_id
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.98 Chrome/71.0.3578.98 Safari/537.36"
+
+def get_guest_token():
+    r = requests.get("https://mobile.twitter.com/jack", headers={"user-agent": USER_AGENT})
+    return r.text.rsplit("(\"gt=", 1)[1].split(";")[0]
+
+
+def fetch_thread(thread_id, guest_token=None):
+    r = requests.get(
+        "https://api.twitter.com/2/timeline/conversation/%i.json" % thread_id,
+        headers={
+            "authorization": "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
+            "x-guest-token": guest_token or get_guest_token(),
+        },
+        params={
+            "include_profile_interstitial_type": 1,
+            "include_blocking": 1,
+            "include_blocked_by": 1,
+            "include_followed_by": 1,
+            "include_want_retweets": 1,
+            "include_mute_edge": 1,
+            "include_can_dm": 1,
+            "include_can_media_tag": 1,
+            "skip_status": 1,
+            "cards_platform": "Web-12",
+            "include_cards": 1,
+            "include_composer_source": "true",
+            "include_ext_alt_text": "true",
+            "include_reply_count": 1,
+            "tweet_mode": "extended",
+            "include_entities": "true",
+            "include_user_entities": "true",
+            "include_ext_media_color": "true",
+            "include_ext_media_availability": "true",
+            "send_error_codes": "true",
+            "count": 200000,
+            "ext": "mediaStats,highlightedLabel,cameraMoment",
+        },
     )
 
-    return [prepare_post(post) for post in r.json()]
+    r.raise_for_status()
+    global_objects = r.json()["globalObjects"]
+    tweets = global_objects["tweets"].values()
+
+    return [
+        prepare_post(post, global_objects) for post in sorted(tweets, key=lambda tweet: tweet["id_str"])
+    ]
 
 
-class TweetEntityParser(ttp.Parser):
-    def format_tag(self, tag, text):
-        """Return formatted HTML for a hashtag."""
-        return '<a href="https://twitter.com/search?q=%s" target="_blank">%s%s</a>' % (
-            ttp.quote(("#" + text).encode("utf-8")),
-            tag,
-            text,
-        )
-
-    def format_username(self, at_char, user):
-        """Return formatted HTML for a username."""
-        return '<a href="https://twitter.com/%s" target="_blank">%s%s</a>' % (
-            user,
-            at_char,
-            user,
-        )
-
-    def format_list(self, at_char, user, list_name):
-        """Return formatted HTML for a list."""
-        return '<a href="https://twitter.com/%s/%s" target="_blank">%s%s/%s</a>' % (
-            user,
-            list_name,
-            at_char,
-            user,
-            list_name,
-        )
-
-    def format_url(self, url, text):
-        """Return formatted HTML for a url."""
-        return '<a href="%s" target="_blank">%s</a>' % (
-            ttp.escape(url),
-            ttp.escape(url),
-        )
-
-
-def shortid(s):
-    s = str(s)
-    return s[:3] + s[-5:]
-
-
-def prepare_post(t):
+def prepare_post(t, global_objects):
     if "full_text" not in t:
+        print("tweet:", t)
         raise ValueError("Non-extended tweet was passed to filter")
 
     start, end = t["display_text_range"]
     text = t["full_text"][start:end]
 
     files = []
+    if t["id_str"] == "1089875279839866880":
+        print(t)
+        print(global_objects["media"])
     if "extended_entities" in t:
         for medium in t["extended_entities"]["media"]:
             text = text.replace(" " + medium["url"], "")
@@ -132,16 +165,59 @@ def prepare_post(t):
     )
     text = TweetEntityParser().parse(text).html
 
+    user = global_objects["users"][t["user_id_str"]]
+
     return {
         "id": t["id_str"],
         "short_id": shortid(t["id_str"]),
         "created_at": t["created_at"],
         "source": t["source"],
-        "user_name": t["user"]["name"],
-        "screen_name": t["user"]["screen_name"],
+        "user_name": user["name"],
+        "screen_name": user["screen_name"],
         "text": text,
         "files": files,
         "thread_op": t["conversation_id_str"],
-        "reply_to": t["in_reply_to_status_id_str"],
-        "short_reply_to": shortid(t["in_reply_to_status_id_str"]),
+        "reply_to": t.get("in_reply_to_status_id_str"),
+        "short_reply_to": shortid(t.get("in_reply_to_status_id_str")),
     }
+
+
+class TweetEntityParser(ttp.Parser):
+    def format_tag(self, tag, text):
+        """Return formatted HTML for a hashtag."""
+        return '<a href="https://twitter.com/search?q=%s" target="_blank">%s%s</a>' % (
+            ttp.quote(("#" + text).encode("utf-8")),
+            tag,
+            text,
+        )
+
+    def format_username(self, at_char, user):
+        """Return formatted HTML for a username."""
+        return '<a href="https://twitter.com/%s" target="_blank">%s%s</a>' % (
+            user,
+            at_char,
+            user,
+        )
+
+    def format_list(self, at_char, user, list_name):
+        """Return formatted HTML for a list."""
+        return '<a href="https://twitter.com/%s/%s" target="_blank">%s%s/%s</a>' % (
+            user,
+            list_name,
+            at_char,
+            user,
+            list_name,
+        )
+
+    def format_url(self, url, text):
+        """Return formatted HTML for a url."""
+        return '<a href="%s" target="_blank">%s</a>' % (
+            ttp.escape(url),
+            ttp.escape(url),
+        )
+
+
+def shortid(s):
+    if s is None: return None
+    s = str(s)
+    return s[:3] + s[-5:]
